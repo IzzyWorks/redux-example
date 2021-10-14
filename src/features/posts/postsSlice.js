708ccd8d5ef28d8a7af02ef35ba51456919d1805
Izzy <izzy.aspler@gmail.com>
2021-10-14 13:54:47 -0700

    dispatch formPost to reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -4,11 +4,16 @@ const initialState = [
   { id: '1', title: 'First Post!', content: 'Hello!' },
   { id: '2', title: 'Second Post', content: 'More text' },
 ]
-
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
-  reducers: {},
+  reducers: {
+    postAdded(state, action) {
+      state.push(action.payload)
+    },
+  },
 })
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
