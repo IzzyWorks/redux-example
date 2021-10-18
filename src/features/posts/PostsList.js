531d83cdc2b6638d948b4bce84525284996fa823
Izzy <izzy.aspler@gmail.com>
2021-10-18 14:44:58 -0700

    added reactions reducer

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -4,9 +4,12 @@ import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const PostsList = () => {
   const posts = useSelector((state) => state.posts)
+
+  // Sort posts in reverse chronological order by datetime string
   const orderedPosts = posts
     .slice()
     .sort((a, b) => b.date.localeCompare(a.date))
@@ -20,6 +23,8 @@ export const PostsList = () => {
           <TimeAgo timestamp={post.date} />
         </div>
         <p className="post-content">{post.content.substring(0, 100)}</p>
+
+        <ReactionButtons post={post} />
         <Link to={`/posts/${post.id}`} className="button muted-button">
           View Post
         </Link>
